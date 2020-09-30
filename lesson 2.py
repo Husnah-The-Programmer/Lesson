4af310cdcb115f730dc807376b0b5c3a8dd11450
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 18:29:51 +0100

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,9 @@
+num1 = 45 
+num2 = 45
+num3 = 60
+
+
+# Add three numbers
+sum = float(num1) + float(num2) + float(num3)
+#Display the sum 
+print('The sum of {0} and {1} and {2} and {3}'.format(num1,num2,num3,sum))
